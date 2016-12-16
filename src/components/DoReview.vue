9f3a321c8bb26aa2e48f4b6c49d7cb211c0a9b8b
Arturo Martinez Minguez <arturo@icemobile.com>
2016-12-16 12:55:47 +0100

    feat: do reviews

--- src/components/DoReview.vue
+++ src/components/DoReview.vue
@@ -1,33 +1,31 @@
 <template>
     <div>
-        <div class="page-header">
-            <h1>Review somebody</h1>
-        </div>
         <div class="loading" v-if="loading">
             Loading...
         </div>
         <div v-if="error" class="alert alert-danger" role="alert">
             <strong>Oh snap!</strong> Something went wrong.
         </div>
-
-        <div class="content cards">
-            <div class="card-container" v-for="card in cards">
-                <div class="card">
-                    <div class="front">
-                        <p>{{card.title}}</p>
-                    </div>
-                    <div class="back">
-                        <p>{{card.category === 0 ? 'This is a GOOD thing, congrats!' : 'This is a BAD thing. Get your sh*t together!'}}</p>
-                    </div>
+        <p class="caption">
+            Please give feedback to your team member by dragging and dropping the cards into the placeholders<br />
+            You can give it a comment by clicking the "i" on the card.
+        </p>
+        <div v-for="row in chunkedCards" class="row cards">
+            <div v-for="card in row" class="col-md-2 cards-container">
+                <div class="card positive">
+                    <div class="card-icon"><span>{{card.category === 0 ? 'Approve' : 'Improve'}}</span></div>
+                    <p class="card-name">{{card.title}}</p>
+                    <a class="card-comment" href="#"><span>Drop a comment</card></a>
                 </div>
             </div>
-
         </div>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import {chunk} from 'lodash';
+
 export default {
     name: 'reviews',
     data() {
@@ -61,58 +59,70 @@ export default {
                 this.error = error;
             });
         }
+    },
+    computed: {
+        chunkedCards() {
+            return chunk(this.cards, 6);
+        }
     }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-/* entire container, keeps perspective */
-.card-container {
-	perspective: 1000px;
-    float: left;
-    margin: 0 1em 1em 0;
-}
-	/* flip the pane when hovered */
-	.card-container:hover .card, .flip-container.hover .card {
-		transform: rotateY(180deg);
-	}
-
-.card-container, .front, .back {
-	width: 160px;
-	height: 240px;
-}
-
-/* flip speed goes here */
 .card {
-	transition: 0.6s;
-	transform-style: preserve-3d;
-
-	position: relative;
+    width: 140px;
+    height: 203px;
+    background-image: url('/static/img/card-background.png');
+    background-repeat: no-repeat;
+    -webkit-box-shadow: -4px 4px 0px 0px #393939;
+    -moz-box-shadow:    -4px 4px 0px 0px #393939;
+    box-shadow:         -4px 4px 0px 0px #393939;
+    margin-bottom: 25px;
 }
-
-/* hide back of pane during swap */
-.front, .back {
-	backface-visibility: hidden;
-	position: absolute;
-	top: 0;
-	left: 0;
-    padding: 1em;
+.card p {
+    color: #000000;
+}
+.positive {
+    background-color: #9fdaf1;
+}
+.negative {
+    background-color: #ffe65d;
+}
+.caption {
+    font-size: 14px;
+    font-family: 'gotham-book';
+    margin-bottom: 25px;
+    margin-top: 25px;
+}
+.card-icon {
+    width: 41px;
+    height: 41px;
+    margin: 0 auto 18px auto;
+    background-image: url('/static/img/thumbs-up.png');
+}
+.card-icon span {
+    display: none;
+}
+.card-name {
+    font-family: 'gotham-black';
+    text-transform: uppercase;
+    font-size: 18px;
     text-align: center;
+    padding-left: 10px;
+    padding-right: 10px;
 }
-
-/* front pane, placed above back */
-.front {
-	z-index: 2;
-	/* for firefox 31 */
-	transform: rotateY(0deg);
-    background-color: lightgreen;
+.card-comment {
+    display: block;
+    background-image: url('/static/img/info.png');
+    width: 21px;
+    height: 20px;
+    position: absolute;
+    bottom: 45px;
+    left: 50%;
+    margin-left: -25px;
 }
-
-/* back, initially hidden pane */
-.back {
-	transform: rotateY(180deg);
-    background-color: lightblue;
+.card-comment span {
+    display: none;
 }
 </style>
